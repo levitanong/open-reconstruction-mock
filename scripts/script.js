9d1e6b1fb5c83cae6e9804ab7070074bc8cb7540
Levi Tan Ong <levi@byimplication.com>
2014-03-22 15:25:01 +0800

    reorders shit

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@
 var publicAPI = function(xhr){
   xhr.withCredentials = false;
 }
+
+////////////////////////////////////////////////////
+// Helpers
+
 var genArray = function($from, $to){
   var list = [];
   if($from < $to){
@@ -16,6 +20,56 @@ var genArray = function($from, $to){
   }
   return list;
 }
+
+var sample = {
+  'disaster names': ['Brunhilda', 'Bantay', 'Muning', 'Dodong', 'Puring'],
+  'disaster': ['Earthquake', 'Flood', 'Typhoon', 'Landslide', 'Anthropogenic'],
+  'projectType': ['Infrastructure', 'Agriculture', 'School Building', 'Health Facilities', 'Shelter Units', 'Environment', 'Other'],
+  'description': [
+    'Adamantium reinforcement for nipa huts.',
+    'These equipments, you know. I need them.',
+    'Very very urgent, we ran out of soft drinks! Help!',
+    'We want to hire Michael Jackson to dance for residents of disaster-afflicted areas',
+    'Request for funding to dispatch search and rescue team for missing local domesticated feline',
+    'Our bridge is missing, can you help us buy another one?'
+  ],
+  'comment': [
+    'Please attach two photocopies of Form R311-78a.9v8 (1983)',
+    'This is a wonderful idea, we should double their budget and give them cake.',
+    'Can I promise to do this if they vote for me?'
+  ],
+  'revision': [null, 0.85, 0.70, 0.65, 0.50, 0.35, 0.2],
+  'region': ["Region 1", "Region 2", "Region 3", "Region 4", "Region 5", "Region 6", "Region 7", "Region 8", "Region 9", "Region 10", "Region 11", "Region 12", "Region 13", 'ARMM', 'NCR', 'CAR']
+};
+
+var rand = {
+  int: function(lower, upper){
+    if(typeof(upper) == 'undefined'){
+      // catches for when only one argument is added, meant for upper.
+      upper = lower;
+      lower = 0
+    }
+    return Math.round(Math.random() * (upper - lower)) + lower;
+  },
+  date: function(backThen){
+    if(typeof(backThen) == "undefined"){
+      var backThen = new Date('January 1, 2013');
+    }
+    var now = new Date(Date.now());
+    return new Date(now - Math.round((now - backThen) * Math.random()));
+  },
+  amount: function(){
+    return Math.round(Math.random() * 100) * 100000;
+  },
+  fromArray: function(arr){
+    var n = Math.random() * (arr.length - 1);
+    var index = Math.round(n);
+    return arr[index];
+  }
+}
+
+////////////////////////////////////////////////////
+// namespace
 
 var recon = {};
 var rand = {};
@@ -111,106 +165,6 @@ var project = {
     };
   }
 }
-
-////////////////////////////////////////////////////
-// Helpers
-
-var sample = {
-  'disaster names': ['Brunhilda', 'Bantay', 'Muning', 'Dodong', 'Puring'],
-  'disaster': ['Earthquake', 'Flood', 'Typhoon', 'Landslide', 'Anthropogenic'],
-  'projectType': ['Infrastructure', 'Agriculture', 'School Building', 'Health Facilities', 'Shelter Units', 'Environment', 'Other'],
-  'description': [
-    'Adamantium reinforcement for nipa huts.',
-    'These equipments, you know. I need them.',
-    'Very very urgent, we ran out of soft drinks! Help!',
-    'We want to hire Michael Jackson to dance for residents of disaster-afflicted areas',
-    'Request for funding to dispatch search and rescue team for missing local domesticated feline',
-    'Our bridge is missing, can you help us buy another one?'
-  ],
-  'comment': [
-    'Please attach two photocopies of Form R311-78a.9v8 (1983)',
-    'This is a wonderful idea, we should double their budget and give them cake.',
-    'Can I promise to do this if they vote for me?'
-  ],
-  'revision': [null, 0.85, 0.70, 0.65, 0.50, 0.35, 0.2],
-  'region': ["Region 1", "Region 2", "Region 3", "Region 4", "Region 5", "Region 6", "Region 7", "Region 8", "Region 9", "Region 10", "Region 11", "Region 12", "Region 13", 'ARMM', 'NCR', 'CAR']
-};
-
-var rand = {
-  int: function(lower, upper){
-    if(typeof(upper) == 'undefined'){
-      // catches for when only one argument is added, meant for upper.
-      upper = lower;
-      lower = 0
-    }
-    return Math.round(Math.random() * (upper - lower)) + lower;
-  },
-  date: function(backThen){
-    if(typeof(backThen) == "undefined"){
-      var backThen = new Date('January 1, 2013');
-    }
-    var now = new Date(Date.now());
-    return new Date(now - Math.round((now - backThen) * Math.random()));
-  },
-  amount: function(){
-    return Math.round(Math.random() * 100) * 100000;
-  },
-  fromArray: function(arr){
-    var n = Math.random() * (arr.length - 1);
-    var index = Math.round(n);
-    return arr[index];
-  }
-}
-
-recon.Calamity = function(data){
-  for(prop in data){
-    this[prop] = m.prop(data[prop]);
-  }
-}
-
-// recon.Action = function(data){
-//   for(prop in data){
-//     this[prop] = m.prop(data[prop]);
-//   }
-// }
-
-// recon.Project = function(data){
-//   for(prop in data){
-//     this[prop] = m.prop(data[prop]);
-//   }
-// }
-
-// recon.Projects = function(){
-//   this.genProject = function(){
-//     return new recon.Project({
-//       date: rand.date(),
-//       level: 1,
-//       isRejected: false,
-//       amount: 0,
-//       description: rand.fromArray(sample.description),
-//       type: rand.fromArray(sample.projectType),
-//       // disaster: {
-//       //   type: self.genFromArray(types.disaster),
-//       //   name: self.genFromArray(types['disaster names']),
-//       //   date: new Date(Date.now()),
-//       //   cause: null
-//       // },
-//       // author: users.list.filter(function(user){return user.level == 0})[0],
-//       // implementingAgency: null,
-//       // location: users.current.address,
-//       // remarks: null,
-//       // history: [],
-//       // attachments: genArray(self.genInt(1, 6))
-//     })
-//   }
-//   this.genProjects = function(qty){
-//     var list = [];
-//     for(var i = 0; i < qty; i++){
-//       list.push(this.genProject());
-//     }
-//     return list;
-//   }
-// }
 
 ////////////////////////////////////////////////////
 // Controller
