8377d77e7be7bcab0259e35d40cdfea8a7ed582d
Levi Tan Ong <levi@byimplication.com>
2014-03-30 22:37:00 +0800

    modifies structure

--- scripts/script.js
+++ scripts/script.js
@@ -605,86 +605,91 @@ projectDetailView.controller = function(){
 projectDetailView.view = function(ctrl){
   var renderErrorList = function(errList){
     if(errList.length){
-      return m("div.row", [
-        m("div.columns.medium-12", [
-          m("h4", "Errors"),
-          errList.map(function(e){
-            return m("span.label.alert", e);
-          })
-        ])
+      return m("span", [
+        m("small", "With errors: "),
+        errList.map(function(e){
+          return m("span.label.alert", e);
+        })
       ])
     }
   }
   return common.main(ctrl,
     m("div#view", [
-      m("div.row", [
-        m("div.columns.medium-12", [
-          m("div.prog", [
-            _.chain(process.steps())
-              .map(function(step, code, list){
-                var progress = ctrl.project().progress();
-                var code = parseInt(code);
-                var width = 100 / _.keys(list).length + "%";
+      m("div.section", [
+        m("div.row", [
+          m("div.columns.medium-12", [
+            m("div.prog", [
+              _.chain(process.steps())
+                .map(function(step, code, list){
+                  var progress = ctrl.project().progress();
+                  var code = parseInt(code);
+                  var width = 100 / _.keys(list).length + "%";
 
-                if(progress > code){
-                  return m("div.step.completed", {style: {width: width}}, step.completed);
-                } else if (progress === code){
-                  return m("div.step.pending", {style: {width: width}}, step.pending);
-                } else {
-                  return m("div.step", {style: {width: width}}, step.label);
-                }
-              })
-              .value()
+                  if(progress > code){
+                    return m("div.step.completed", {style: {width: width}}, step.completed);
+                  } else if (progress === code){
+                    return m("div.step.pending", {style: {width: width}}, step.pending);
+                  } else {
+                    return m("div.step", {style: {width: width}}, step.label);
+                  }
+                })
+                .value()
+            ])
+          ]),
+        ]),
+        m("div.row", [
+          m("div.columns.medium-12", [
+            m("h4", [
+              "Posted by "+ctrl.project().author().name+" on "+ctrl.project().date().toDateString() + " ", // change this as people modify this. "Last edited by _____"
+              renderErrorList(ctrl.project().errors())
+            ]),
+            m("h1", ctrl.project().description()),
+            
           ])
         ]),
-      ]),
-      m("div.row", [
-        m("div.columns.medium-12", [
-          m("h4", "Posted by "+ctrl.project().author().name+" on "+ctrl.project().date().toDateString()),
-          m("h1", ctrl.project().description()),
+        m("div.row", [
+          m("div.columns.medium-3", [
+            m("h4", [m("small", "Amount")]),
+            common.renderString(
+              helper.commaize(ctrl.project().amount())
+            )
+          ]),
+          m("div.columns.medium-3", [
+            m("h4", [m("small", "Type")]),
+            ctrl.project().type()
+          ]),
+          m("div.columns.medium-3", [
+            m("h4", [m("small", "Disaster")]),
+            common.renderString(ctrl.project().disaster().type + " " + ctrl.project().disaster().name + ", in " + ctrl.project().disaster().date)
+          ]),
+          m("div.columns.medium-3", [
+            m("h4", [m("small", "Location")]),
+            common.renderString(
+              _.chain(ctrl.project().location())
+              .filter(function(entry){
+                return entry
+              })
+              .reduce(function(memo, next){
+                if(!memo){
+                  return next;
+                } else {
+                  return memo + ", " + next;
+                }
+              }, "")
+              .value()
+            )
+          ]),
         ])
       ]),
-      m("div.row", [
-        m("div.columns.medium-3", [
-          m("h4", "Amount"),
-          common.renderString(
-            helper.commaize(ctrl.project().amount())
-          )
-        ]),
-        m("div.columns.medium-3", [
-          m("h4", "Type"),
-          ctrl.project().type()
-        ]),
-        m("div.columns.medium-3", [
-          m("h4", "Disaster"),
-          common.renderString(ctrl.project().disaster().type + " " + ctrl.project().disaster().name + ", in " + ctrl.project().disaster().date)
-        ]),
-        m("div.columns.medium-3", [
-          m("h4", "Location"),
-          common.renderString(
-            _.chain(ctrl.project().location())
-            .filter(function(entry){
-              return entry
-            })
-            .reduce(function(memo, next){
-              if(!memo){
-                return next;
-              } else {
-                return memo + ", " + next;
-              }
-            }, "")
-            .value()
-          )
-        ])
-      ]),
-      renderErrorList(ctrl.project().errors()),
       m("hr"),
-      m("div.row", [
-        m("div.columns.medium-9", [
-          "conversations"
-        ]),
-        m("div.columns.medium-3", [
-          "attachment list"
+      m("div.section.history", [
+        m("div.row", [
+          m("div.columns.medium-9", [
+            "conversations"
+          ]),
+          m("div.columns.medium-3", [
+            "attachment list"
+          ])
         ])
       ])
     ])
