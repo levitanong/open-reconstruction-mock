7a5cddab052fb7603ec09c96b928b6145e233de9
Levi Tan Ong <levi@byimplication.com>
2014-03-30 23:02:57 +0800

    more refinements

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,47 @@
-////////////////////////////////////////////////////
-// Helpers
+// ////////////////////////////////////////////////////
+// // Helpers
 
-var genArray = function($from, $to){
-  var list = [];
-  if($from < $to){
-    for(var i = $from; i < $to; i++){
-      list.push(i);
-    }
-  } else {
-    // $from >= $to
-    for (var i = $from; i > $to; i--){
-      list.push(i);
-    }
-  }
-  return list;
-}
+// var genArray = function($from, $to){
+//   var list = [];
+//   if($from < $to){
+//     for(var i = $from; i < $to; i++){
+//       list.push(i);
+//     }
+//   } else {
+//     // $from >= $to
+//     for (var i = $from; i > $to; i--){
+//       list.push(i);
+//     }
+//   }
+//   return list;
+// }
 
-var rand = {
-  int: function(lower, upper){
-    if(typeof(upper) == 'undefined'){
-      // catches for when only one argument is added, meant for upper.
-      upper = lower;
-      lower = 0
-    }
-    return Math.round(Math.random() * (upper - lower)) + lower;
-  },
-  date: function(backThen){
-    if(typeof(backThen) == "undefined"){
-      var backThen = new Date('January 1, 2013');
-    }
-    var now = new Date(Date.now());
-    return new Date(now - Math.round((now - backThen) * Math.random()));
-  },
-  amount: function(){
-    return Math.round(Math.random() * 100) * 100000;
-  },
-  fromArray: function(arr){
-    return _.sample(arr, 1)[0]
-    // var n = Math.random() * (arr.length - 1);
-    // var index = Math.round(n);
-    // return arr[index];
-  }
-}
+// var rand = {
+//   int: function(lower, upper){
+//     if(typeof(upper) == 'undefined'){
+//       // catches for when only one argument is added, meant for upper.
+//       upper = lower;
+//       lower = 0
+//     }
+//     return Math.round(Math.random() * (upper - lower)) + lower;
+//   },
+//   date: function(backThen){
+//     if(typeof(backThen) == "undefined"){
+//       var backThen = new Date('January 1, 2013');
+//     }
+//     var now = new Date(Date.now());
+//     return new Date(now - Math.round((now - backThen) * Math.random()));
+//   },
+//   amount: function(){
+//     return Math.round(Math.random() * 100) * 100000;
+//   },
+//   fromArray: function(arr){
+//     return _.sample(arr, 1)[0]
+//     // var n = Math.random() * (arr.length - 1);
+//     // var index = Math.round(n);
+//     // return arr[index];
+//   }
+// }
 
 var helper = {
   truncate: function(input, place){
@@ -111,27 +111,6 @@ var helper = {
     }, "");
   }
 }
-
-// var sample = {
-//   'disaster names': ['Brunhilda', 'Bantay', 'Muning', 'Dodong', 'Puring'],
-//   'disaster': ['Earthquake', 'Flood', 'Typhoon', 'Landslide', 'Anthropogenic'],
-//   'projectType': ['Infrastructure', 'Agriculture', 'School Building', 'Health Facilities', 'Shelter Units', 'Environment', 'Other'],
-//   'description': [
-//     'Adamantium reinforcement for nipa huts.',
-//     'These equipments, you know. I need them.',
-//     'Very very urgent, we ran out of soft drinks! Help!',
-//     'We want to hire Michael Jackson to dance for residents of disaster-afflicted areas',
-//     'Request for funding to dispatch search and rescue team for missing local domesticated feline',
-//     'Our bridge is missing, can you help us buy another one?'
-//   ],
-//   'comment': [
-//     'Please attach two photocopies of Form R311-78a.9v8 (1983)',
-//     'This is a wonderful idea, we should double their budget and give them cake.',
-//     'Can I promise to do this if they vote for me?'
-//   ],
-//   'revision': [null, 0.85, 0.70, 0.65, 0.50, 0.35, 0.2],
-//   'region': ["Region 1", "Region 2", "Region 3", "Region 4", "Region 5", "Region 6", "Region 7", "Region 8", "Region 9", "Region 10", "Region 11", "Region 12", "Region 13", 'ARMM', 'NCR', 'CAR']
-// };
 
 ////////////////////////////////////////////////////
 // fake database
@@ -497,6 +476,17 @@ common.renderObj = function(obj){
       })
       .value();
   }
+}
+
+common.historyEvent = function(data){
+  return m(".event", [
+    m(".date", [
+
+    ]),
+    m(".details", [
+      
+    ])
+  ])
 }
 
 projectListView = {};
