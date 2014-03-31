56aaa4bddd0ba51a3cd2cfc020d981d1aa396d30
Levi Tan Ong <levi@byimplication.com>
2014-03-31 19:14:42 +0800

    timeago, history events

--- scripts/script.js
+++ scripts/script.js
@@ -109,7 +109,66 @@ var helper = {
         return acc + "," + head;
       }
     }, "");
-  }
+  },
+  timeago: function (time, local, raw) {
+    //time: the time
+    //local: compared to what time? default: now
+    //raw: wheter you want in a format of "5 minutes ago", or "5 minutes"
+    if (!time) return "never";
+
+    if (!local) {
+      local = new Date(Date.now());
+    }
+
+    // assume that time is of type Date
+    time = time.getTime();
+    // if (angular.isDate(time)) {
+    //   time = time.getTime();
+    // } else if (typeof time === "string") {
+    //   time = new Date(time).getTime();
+    // }
+
+    // assume that local is of type Date
+    local = local.getTime();
+    // if (angular.isDate(local)) {
+    //   local = local.getTime();
+    // }else if (typeof local === "string") {
+    //   local = new Date(local).getTime();
+    // }
+
+    if (typeof time !== 'number' || typeof local !== 'number') {
+      return;
+    }
+
+    var
+      offset = Math.abs((local - time) / 1000),
+      span = [],
+      MINUTE = 60,
+      HOUR = 3600,
+      DAY = 86400,
+      WEEK = 604800,
+      MONTH = 2629744,
+      YEAR = 31556926,
+      DECADE = 315569260;
+
+    if (offset <= MINUTE)              span = [ '', raw ? 'now' : 'less than a minute' ];
+    else if (offset < (MINUTE * 60))   span = [ Math.round(Math.abs(offset / MINUTE)), 'min' ];
+    else if (offset < (HOUR * 24))     span = [ Math.round(Math.abs(offset / HOUR)), 'hr' ];
+    else if (offset < (DAY * 7))       span = [ Math.round(Math.abs(offset / DAY)), 'day' ];
+    else if (offset < (WEEK * 52))     span = [ Math.round(Math.abs(offset / WEEK)), 'week' ];
+    else if (offset < (YEAR * 10))     span = [ Math.round(Math.abs(offset / YEAR)), 'year' ];
+    else if (offset < (DECADE * 100))  span = [ Math.round(Math.abs(offset / DECADE)), 'decade' ];
+    else                               span = [ '', 'a long time' ];
+
+    span[1] += (span[0] === 0 || span[0] > 1) ? 's' : '';
+    span = span.join(' ');
+
+    if (raw === true) {
+      return span;
+    }
+    return (time <= local) ? span + ' ago' : 'in ' + span;
+  },
+  monthArray: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
 }
 
 ////////////////////////////////////////////////////
@@ -479,14 +538,19 @@ common.renderObj = function(obj){
 }
 
 common.historyEvent = function(data){
-  return m(".event", [
-    m(".date", [
-      data.date
-    ]),
-    m(".details", [
-      m("p", data.type),
-      m("p", data.name)
+  var dateCircle = function(date){
+    return m(".date", [
+      m("div.month", helper.monthArray[data.date.getMonth()]),
+      m("h4.day", data.date.getDate()),
+      m("div.year", data.date.getFullYear())
     ])
+  }
+  return m(".event", [
+    dateCircle(data.date),
+    m(".details", [
+      m("h3", data.type + " " + data.name),
+      m("p", helper.timeago(data.date))
+    ]),
   ])
 }
 
