f969074e979f865ff719d7906ae642d5f485c46a
Levi Tan Ong <levi@byimplication.com>
2014-04-01 23:34:59 +0800

    adds titling for list of projects requested by user

--- scripts/app/user/user.view.js
+++ scripts/app/user/user.view.js
@@ -1,9 +1,9 @@
 user.view = function(ctrl){
   return common.main(ctrl, m("div#view", [
     common.banner(ctrl.user().name),
-    // console.log(ctrl.projectList()),
     m("div", {class: "row"}, [
       m("div", {class: "columns medium-9"}, [
+        m("h1",[m("small", "List of projects requested by this user")]),
         project.listView(ctrl)
       ])
     ])
