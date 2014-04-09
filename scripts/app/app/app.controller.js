7e5c0a306c23c6e5e2d25b74e4f0328748f12469
Levi Tan Ong <levi@byimplication.com>
2014-04-09 09:45:25 +0800

    factors out map

--- scripts/app/app/app.controller.js
+++ scripts/app/app/app.controller.js
@@ -15,6 +15,20 @@ app.controller = function(){
       return user.department === "NDRRMC";
     });
   };
+  this.initMap = function(elem, config){
+    var map = L.map(elem, config).setView([11.3333, 123.0167], 9);
+
+    // create the tile layer with correct attribution
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});   
+
+    // start the map in South-East England
+    // map.setView(new L.LatLng(51.3, 0.7),9);
+    map.addLayer(osm);
+
+    L.tileLayer(osmUrl, {attribution: osmAttrib, maxZoom: 18}).addTo(map);
+  };
   this.db = {};
   this.db.clear = function(){
     localStorage.clear();
--- scripts/app/projectCreation/projectCreation.controller.js
+++ scripts/app/projectCreation/projectCreation.controller.js
@@ -4,18 +4,7 @@ projectCreation.controller = function(){
   this.scopeOfWork = m.prop("Reconstruction");
   this.initMap = function(elem){
     if(!elem.classList.contains("leaflet-container")){
-      var map = L.map(elem, {drawControl: true, scrollWheelZoom: false}).setView([51.505, -0.09], 13);
-
-      // create the tile layer with correct attribution
-      var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-      var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-      var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});   
-
-      // start the map in South-East England
-      map.setView(new L.LatLng(51.3, 0.7),9);
-      map.addLayer(osm);
-
-      L.tileLayer(osmUrl, {attribution: osmAttrib, maxZoom: 18}).addTo(map);
+      this.app.initMap(elem, {drawControl: true, scrollWheelZoom: false});
     }
   }
 }
